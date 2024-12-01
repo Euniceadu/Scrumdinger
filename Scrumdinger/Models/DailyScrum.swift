bacc49aae123bcd2bba3aaa121a9962937c4395f
Euniceadu <eunice.obugyei@gmail.com>
2024-12-01 00:27:40 +0000

    Scrum Meeting List Design Worked on design for meeting lists

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -5,17 +5,30 @@
 //  Created by Eunice Obugyei on 26/11/2024.
 //
 
-struct DailyScrum {
+import Foundation
+
+struct DailyScrum: Identifiable {
+    var id: UUID
     var title: String
     var attendees: [String]
     var lengthInMinutes: Int
     var theme: Theme
+    
+    init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
 }
 
 extension DailyScrum {
     static let sampleData: [DailyScrum] = [
         DailyScrum(title: "iOS Dev", attendees: ["Anna", "Jace", "Steve"], lengthInMinutes: 15, theme: .yellow),
-        DailyScrum(title: "API Dev", attendees: ["Joana", "Amma", "Winnie"], lengthInMinutes: 15, theme: .tan)
+        DailyScrum(title: "API Dev", attendees: ["Joana", "Amma", "Winnie"], lengthInMinutes: 15, theme: .purple),
+        DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
+
         
     ]
 }
---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,22 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Eunice Obugyei on 01/12/2024.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(dailyScrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
